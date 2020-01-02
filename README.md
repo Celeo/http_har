c1d9b66a002e5c3fe4f5200fb392958034eb9456
Celeo <celeodor@gmail.com>
2020-01-01 21:36:39 -0800

    Progress

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # http_har
 
-A [Nim](https://nim-lang.org/) library to turn request and responses from the [standard library](https://nim-lang.org/docs/httpclient.html) into [HAR](https://en.wikipedia.org/wiki/HAR_(file_format)) documents.
+A [Nim](https://nim-lang.org/) library to turn request and responses from the [standard library](https://nim-lang.org/docs/httpclient.html) into [HAR](https://en.wikipedia.org/wiki/HAR_(file_format)) v1.2 documents.
 
 ## Usage
 
--- src/http_har.nim
+++ src/http_har.nim
@@ -1,1 +1,71 @@
-# TODO
+import asynchttpserver,
+  httpclient,
+  httpcore,
+  json,
+  strutils,
+  times
+
+proc start(): JsonNode =
+  %*{
+    "log": {
+      "version": "1.2",
+      "creator": {
+        "name": "",
+        "version": ""
+      },
+      "entries": []
+    }
+  }
+
+proc convertHeaders(headers: HttpHeaders): (seq[JsonNode], int) =
+  var headers: seq[JsonNode] = @[]
+  for (k, v) in headers.pairs():
+    headers.add(%*{
+      "name": k,
+      "value": v
+    })
+  # TODO calculate size
+  (headers, 0)
+
+proc convert*(request: Request): JsonNode =
+  let (headers, headerSize) = convertHeaders(request.headers)
+  %*{
+    "bodySize": request.body.len(),
+    "method": $request.reqMethod,
+    "url": $request.url,
+    "httpVersion": "HTTP/1.1",
+    "headers": headers,
+    "headersSize": headerSize,
+    "cookies": [], # TODO
+    "queryString": [] # TODO, data in 'request.query'
+  }
+
+proc convert*(response: Response): JsonNode =
+  let (headers, headerSize) = convertHeaders(response.headers)
+  %*{
+    "status": response.status.parseInt(),
+    "statusText": ($response.code()).split(" ")[1..^1].join(" "),
+    "httpVersion": "HTTP/1.1",
+    "headers": headers,
+    "headersSize": headerSize,
+    "cookies": [], # TODO
+    "content": {}, # TODO
+    "bodySize": 0, # TODO
+    "redirectURL": ""
+  }
+
+proc convert*(request: Request, response: Response): string =
+  var data = start()
+  var entry = %*{
+    "startedDateTime": now().format("yyyy-MM-dd'T'hh:mm:ss'.0'zzz"),
+    "request": convert(request),
+    "response": convert(response),
+    "cache": {},
+    "timings": {
+      "send": 0,
+      "wait": 0,
+      "receive": 0,
+    }
+  }
+  data["entries"].add(entry)
+  $data
