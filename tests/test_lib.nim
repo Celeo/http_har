81f26744deec939242b3ff35caaf9c62e1c6a4c8
Celeo <celeodor@gmail.com>
2020-01-03 17:59:42 -0800

    more progress; sorting out weird runtime errors
    [ci skip]

--- tests/test_lib.nim
+++ tests/test_lib.nim
@@ -35,59 +35,70 @@ proc getAsyncResponse(): AsyncResponse =
   result.headers.add("content-type", "application/json")
   result.bodyStream = newFutureStream[string]("'Hello world'")
 
-test "start":
-  let s = start()
-  check:
-    s["log"]["version"].getStr() == "1.2"
+suite "common":
 
-test "convertHeaders":
-  let headers = newHttpHeaders(@[("foo", "bar"), ("baz", "123")])
-  let converted = convertHeaders(headers)
-  check:
-    converted[0]["name"].getStr() == "foo"
-    converted[0]["value"].getStr() == "bar"
-    converted[1]["name"].getStr() == "baz"
-    converted[1]["value"].getStr() == "123"
+  test "start":
+    let s = start()
+    check:
+      s["log"]["version"].getStr() == "1.2"
 
-test "convertCookies":
-  let headers = newHttpHeaders(@[("something", "else"), ("Cookie", "foo=bar; baz=123")])
-  let converted = convertCookies(headers)
-  check:
-    converted[0]["name"].getStr() == "foo"
-    converted[0]["value"].getStr() == "bar"
-    converted[1]["name"].getStr() == "baz"
-    converted[1]["value"].getStr() == "123"
+  test "convertHeaders":
+    let headers = newHttpHeaders(@[("foo", "bar"), ("baz", "123")])
+    let converted = convertHeaders(headers)
+    check:
+      converted[0]["name"].getStr() == "foo"
+      converted[0]["value"].getStr() == "bar"
+      converted[1]["name"].getStr() == "baz"
+      converted[1]["value"].getStr() == "123"
 
-test "convertQueryString":
-  let u = parseUri("http://localhost?foo=bar&baz=123")
-  let converted = convertQueryString(u)
-  check:
-    converted[0]["name"].getStr() == "foo"
-    converted[0]["value"].getStr() == "bar"
-    converted[1]["name"].getStr() == "baz"
-    converted[1]["value"].getStr() == "123"
+  test "convertCookies":
+    let headers = newHttpHeaders(@[("something", "else"), ("Cookie", "foo=bar; baz=123")])
+    let converted = convertCookies(headers)
+    check:
+      converted[0]["name"].getStr() == "foo"
+      converted[0]["value"].getStr() == "bar"
+      converted[1]["name"].getStr() == "baz"
+      converted[1]["value"].getStr() == "123"
 
-test "convert - request":
-  let converted = convert(getRequest())
-  let expected = parseJson("""{"bodySize":13,"method":"POST","url":"https://example.com?foo=bar&baz=123","httpVersion":"HTTP/1.1","headers":[{"name":"content-type","value":"application/json"}],"headersSize":-1,"cookies":[],"queryString":[{"name":"foo","value":"bar"},{"name":"baz","value":"123"}],"postData":{"mimeType":"application/json","params":[],"text":"'hello world'"}}""")
-  check:
-    converted == expected
+  test "convertQueryString":
+    let u = parseUri("http://localhost?foo=bar&baz=123")
+    let converted = convertQueryString(u)
+    check:
+      converted[0]["name"].getStr() == "foo"
+      converted[0]["value"].getStr() == "bar"
+      converted[1]["name"].getStr() == "baz"
+      converted[1]["value"].getStr() == "123"
 
-test "convert - response":
-  let converted = convert(getResponse())
-  let expected = parseJson("""{"status":200,"statusText":"OK","httpVersion":"HTTP/1.1","headers":[{"name":"content-type","value":"application/json"}],"headersSize":-1,"cookies":[],"content":{"size":13,"compression":0,"mimeType":"application/json","text":"'Hello world'"},"bodySize":13,"redirectURL":""}""")
-  check:
-    converted == expected
+suite "sync":
 
-test "convertResponse - async response":
-    # FIXME this is failing in the async bit
-  let converted = waitFor(convertAsync(getAsyncResponse()))
-  let expected = parseJson("""{"status":200,"statusText":"OK","httpVersion":"HTTP/1.1","headers":[{"name":"content-type","value":"application/json"}],"headersSize":-1,"cookies":[],"content":{"size":13,"compression":0,"mimeType":"application/json","text":"'Hello world'"},"bodySize":13,"redirectURL":""}""")
-  check:
-    converted == expected
+  test "convert - request":
+    let converted = convert(getRequest())
+    let expected = parseJson("""{"bodySize":13,"method":"POST","url":"https://example.com?foo=bar&baz=123","httpVersion":"HTTP/1.1","headers":[{"name":"content-type","value":"application/json"}],"headersSize":-1,"cookies":[],"queryString":[{"name":"foo","value":"bar"},{"name":"baz","value":"123"}],"postData":{"mimeType":"application/json","params":[],"text":"'hello world'"}}""")
+    check:
+      converted == expected
 
-test "convertFull":
-  let converted = convert(getRequest(), getResponse())
-  let asJson = parseJson(converted)
-  check:
-    "log" in asJson
+  test "convert - response":
+    let converted = convert(getResponse())
+    let expected = parseJson("""{"status":200,"statusText":"OK","httpVersion":"HTTP/1.1","headers":[{"name":"content-type","value":"application/json"}],"headersSize":-1,"cookies":[],"content":{"size":13,"compression":0,"mimeType":"application/json","text":"'Hello world'"},"bodySize":13,"redirectURL":""}""")
+    check:
+      converted == expected
+
+  test "convert - request and response":
+    let converted = convert(getRequest(), getResponse())
+    let asJson = parseJson(converted)
+    check:
+      "log" in asJson
+
+suite "async":
+
+  test "convertResponse - response, async":
+    # FIXME this line is throwing an exception
+    let converted: JsonNode = waitFor(convertAsync(getAsyncResponse()))
+
+    echo converted
+
+    # ...
+
+  test "convert - request and response, async":
+    # TODO
+    discard
